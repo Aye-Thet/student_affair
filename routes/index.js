9a19b80ccdaf6f82ab9026fd61939a7928ebd2d0
Aye-Thet <ayethet107@gmail.com>
2018-08-18 11:36:57 +0630

    duplicate email for sign up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var User = require('../models/User')
+var User = require('../models/User');
 
 // /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,14 +13,42 @@ router.get('/home', function(req, res, next) {
   res.render('Home');
 });
 
-/* login page. */
-router.get('/login', function(req, res, next) {
-  res.render('commons/login', {title: 'Login Form'});
-});
-
-/* signup page. */
+/* GET signup page. */
 router.get('/signup', function(req, res, next) {
   res.render('commons/signup', {title: 'Sign Up Page'});
+});
+
+/* POST signup page. */
+router.post('/signup', function(req, res, next) {
+  var params = [req.body.name, req.body.email, req.body.password];
+  User.findByEmail(req.body.email, function(err, rows){
+    if(err) throw err;
+    if(rows.length > 0){
+      req.flash('warn', 'Duplicated email!');
+      res.redirect('/signup');
+    }else{
+      User.add(params, function(err2, result){
+        if(err2) throw err2;
+        res.redirect('/login')
+      });
+    }
+  });
+});
+
+router.post('/dupemail', function(req, res, next){
+  User.findByEmail(req.body.email, function(err, rows){
+    if(err) throw err;
+    if(rows.length > 0){
+      res.json({ status: true, msg: 'Duplicated email!'});
+    }else{
+      res.json({ status: false});
+    }
+  });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('commons/login', {title: 'Login Form'});
 });
 
 /* IT . */
@@ -68,5 +96,11 @@ router.get('/old', function(req, res, next) {
 router.get('/list', function(req, res, next) {
   res.render('student/list');
 });
+
+// /*GET Test register. */
+// router.get('/sample', function(req, res, next) {
+//   res.render('commons/sample');
+// });
+
 
 module.exports = router;
